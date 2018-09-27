58ba029db472281e1c990c5d927be6d5983c4ccf
plusline <jason12399@gmail.com>
2018-09-27 18:02:47 +0800

    Implement insert

--- queue.c
+++ queue.c
@@ -40,11 +40,12 @@ void q_free(queue_t *q)
     while (current != NULL) {
         temp = current;
         current = current->next;
-        printf("value: %s\n", current->value);
-        free(current->value);
+        printf("%s\n", temp->value);
+        free(temp->value);
         free(temp);
     }
-    free(q);
+    if (q != NULL)
+        free(q);
 }
 
 /*
@@ -57,10 +58,16 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
+    if (q == NULL) {
+        return false;
+    }
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    newh->value = malloc(sizeof(50));
+    strcpy(newh->value, s);
+
     newh->next = q->head;
     q->head = newh;
     return true;
